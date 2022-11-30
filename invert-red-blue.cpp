2791f5677c84ccc665e22f74da52585ce266b87e
romain22222 <romrom22222@hotmail.fr>
2022-11-30 16:10:25 +0100

    Q6

--- invert-red-blue.cpp
+++ invert-red-blue.cpp
@@ -17,15 +17,9 @@ int main(int argc, char** argv )
 	std::ifstream input( fn );
 	ColorImage2D img = Image2DWriter<Color>::read(input);
 	input.close();
-	std::cout << img.w() << " " << img.h() << " " << img.at(4,5).r() << " " << img.at(4,5).g() << " " << img.at(4,5).b() << std::endl;
-
 	for (Iterator it = img.begin(), itE = img.end(); it != itE; ++it ) {
-		std::cout << (*it).b() << (*it).g()<< (*it).r() << std::endl;
 		*it = Color(it->blue, it->green, it->red);
-		std::cout << (*it).b() << (*it).g()<< (*it).r() << std::endl;
 	}
-	std::cout << img.w() << " " << img.h() << " " << img.at(4,5).r() << " " << img.at(4,5).g() << " " << img.at(4,5).b() << std::endl;
-
 	std::ofstream output("rbinverted-" + fn);
 	Image2DWriter<Color>::write(img, output, false);
 	return 0;
